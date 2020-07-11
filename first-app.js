80f381b38fe8d704f63d9d04c4d0bb86fa306efc
thisbutterfly <berianushka@gmail.com>
2020-07-12 04:14:30 +0530

    initial setup

---
+++ first-app.js
@@ -0,0 +1,5 @@
+// console.log('Hello this is Anushka!')
+
+const fs = require('fs');
+
+fs.writeFileSync('Hello.txt', 'Hello from NodeJs'); //creates a new file if the specified file does not exist.
