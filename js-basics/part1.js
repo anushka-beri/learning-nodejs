9a7ae99b6b3d202f95d97a1d49b3da7795c27ca5
thisbutterfly <berianushka@gmail.com>
2020-07-13 00:37:21 +0530

    var and functions

---
+++ js-basics/part1.js
@@ -0,0 +1,16 @@
+var name = "Anushka";
+var age = 22;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user had hobbies " +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
